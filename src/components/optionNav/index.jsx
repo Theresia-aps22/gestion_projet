d98a8afd7b2bf8247a0f6627dc9ae926251f31c4
unknown <theresia22.aps2a@gmail.com>
2024-06-23 23:25:11 +0300

    modif

--- src/components/optionNav/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function OptionNav(){
-    return(
-        <li className="nav-item">
-            <a className="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-            <i className="fas fa-th-large"></i>
-            </a>
-        </li>
-    )
-}
